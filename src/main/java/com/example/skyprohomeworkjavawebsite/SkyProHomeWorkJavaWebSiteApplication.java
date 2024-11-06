b3aa15f40ae6204ef21e63ce402f6d67ae7705cf
Med771 <mm7737333@gmail.com>
2024-11-06 12:48:12 +0300

    Begin

---
+++ src/main/java/com/example/skyprohomeworkjavawebsite/SkyProHomeWorkJavaWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.skyprohomeworkjavawebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProHomeWorkJavaWebSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProHomeWorkJavaWebSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skyprohomeworkjavawebsite/SkyProHomeWorkJavaWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyprohomeworkjavawebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProHomeWorkJavaWebSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
